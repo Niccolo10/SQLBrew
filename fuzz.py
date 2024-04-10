4752c86cdcb4e42cd10360bbe15cdff4353e228c
whitek <parlanti.niccolo@gmail.com>
2024-04-10 15:48:02 +0200

    format code

--- fuzz.py
+++ fuzz.py
@@ -1,11 +1,10 @@
-from db.db_connector import connect_to_mysql, load_db_config, logger
-import random
+# import random
 from mysql.connector import Error
-import constraint_oracle as co
 
-add_employee = ("INSERT INTO employees "
-               "(name, age) "
-               "VALUES (%s, %s)")
+import constraint_oracle as co
+from db.db_connector import connect_to_mysql, load_db_config, logger
+
+add_employee = "INSERT INTO employees " "(name, age) " "VALUES (%s, %s)"
 
 # Load the configuration
 config = load_db_config()
@@ -15,14 +14,17 @@ oracle = co.ConstraintOracle()
 if config is not None:
     try:
         cnx = connect_to_mysql(config)
-        
+
         if cnx is not None:
             logger.info("Starting fuzzing operations...")
             cursor = cnx.cursor()
 
             for i in range(5):
                 mutated_value = 17.99999999999999
-                data_employee = ('employee{i}', mutated_value)  # Fixed typo in 'employee'
+                data_employee = (
+                    "employee{i}",
+                    mutated_value,
+                )  # Fixed typo in 'employee'
                 answer = oracle.check_constraint(mutated_value)
 
                 try:
@@ -31,14 +33,22 @@ if config is not None:
                     cnx.commit()
 
                     # Log insertion info with oracle's evaluation
-                    logger.info(f"Inserted new employee with ID: {cursor.lastrowid} - Oracle evaluation: {answer}")
-                    if answer == False:
-                        logger.info(f"Mutated value: {mutated_value} - Oracle evaluation: {answer}") #BUUUUGGGGGG
+                    logger.info(
+                        f"Inserted new employee with ID: {cursor.lastrowid} - Oracle evaluation: {answer}"
+                    )
+                    if answer is False:
+                        logger.info(
+                            f"Mutated value: {mutated_value} - Oracle evaluation: {answer}"
+                        )  # BUUUUGGGGGG
                 except Error as e:
                     # Log the error and continue with the next iteration
-                    logger.error(f"An error occurred during the insertion of the new employee: {e} - Oracle evaluation: {answer}")
-                    if answer == True:
-                        logger.info(f"Mutated value: {mutated_value} - Oracle evaluation: {answer}") #BUUUUGGGGGG
+                    logger.error(
+                        f"An error occurred during the insertion of the new employee: {e} - Oracle evaluation: {answer}"
+                    )
+                    if answer is True:
+                        logger.info(
+                            f"Mutated value: {mutated_value} - Oracle evaluation: {answer}"
+                        )  # BUUUUGGGGGG
                     continue
 
             # Remember to close the cursor and connection after operations
--- grammar/solver.py
+++ grammar/solver.py
@@ -1,44 +1,52 @@
-from typing import List, Dict, Union, Any, Tuple
+from typing import Any, Dict, List, Tuple, Union  # noqa: F401
+
+from isla.derivation_tree import DerivationTree
 from isla.solver import ISLaSolver
 from isla.type_defs import Grammar
-from isla.derivation_tree import DerivationTree
 from tree import display_tree
 
-fake = DerivationTree( "<start>" )
+fake = DerivationTree("<start>")
+
 
 class TableGrammar:
     def __init__(self) -> None:
-
-        self.GRAMMAR : Grammar = {
+        self.GRAMMAR: Grammar = {
             "<start>": ["CREATE TABLE <table-name> (<columns-and-constraints>);"],
             "<table-name>": ["t1"],
             "<columns-and-constraints>": [
                 "<column>, CONSTRAINT <constraint-name> CHECK (<check-condition>)",
-                "<column>, <column>, CONSTRAINT <constraint-name> CHECK (<compare-two-columns>)"
+                "<column>, <column>, CONSTRAINT <constraint-name> CHECK (<compare-two-columns>)",
             ],
             "<column>": ["<column-name> <data-type>"],
             "<column-name>": ["c1", "c2"],
             "<data-type>": ["INT", "VARCHAR(255)", "BOOLEAN", "FLOAT"],
             "<constraint-name>": ["v1"],
-            "<check-condition>": ["c1 > <digits>", "CHAR_LENGTH(c1) > <digits>", "c1 LIKE '%@%.%'"],
-            "<compare-two-columns>": ["c1 < c2", "c1 > c2", "CHAR_LENGTH(c1) > CHAR_LENGTH(c2)"],
+            "<check-condition>": [
+                "c1 > <digits>",
+                "CHAR_LENGTH(c1) > <digits>",
+                "c1 LIKE '%@%.%'",
+            ],
+            "<compare-two-columns>": [
+                "c1 < c2",
+                "c1 > c2",
+                "CHAR_LENGTH(c1) > CHAR_LENGTH(c2)",
+            ],
             "<digits>": ["<digit><digits>", "<digit>"],
-            "<digit>": list("0123456789")
+            "<digit>": list("0123456789"),
         }
 
-        self.constraints = ''' str.to.int(<digit>) > 5'''
+        self.constraints = """ str.to.int(<digit>) > 5"""
 
     def solve(self, max_free_instantiations=10, max_smt_instantiations=10):
-
         solver = ISLaSolver(
             grammar=self.GRAMMAR,
-            #formula=self.constraints,
+            # formula=self.constraints,
             max_number_free_instantiations=max_free_instantiations,
             max_number_smt_instantiations=max_smt_instantiations,
-            debug=True
+            debug=True,
         )
         solutions = []
-        
+
         try:
             for _ in range(1):  # Adjust the number of attempts as needed
                 solution = solver.solve()
@@ -50,12 +58,15 @@ class TableGrammar:
             pass  # Handle the case when no more unique solutions can be generated
         return solutions
 
+
 # Example usage:
 table = TableGrammar()
 solutions = table.solve()
 for solution in solutions:
-    print((solution)) # str(solution) if i want the string -> now is returning DerivationTree / print implicity transform in string
+    print(
+        (solution)
+    )  # str(solution) if i want the string -> now is returning DerivationTree / print implicity transform in string
     dot = display_tree(solution)
-    dot.render('output_filename', view=True)
+    dot.render("output_filename", view=True)
 
-    #print("ciao")
+    # print("ciao")
